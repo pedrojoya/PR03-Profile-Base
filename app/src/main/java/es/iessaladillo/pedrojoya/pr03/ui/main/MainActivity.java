af23f699106d6b105dce41133bfb6022ae8a6eb1
Yeray <yeray3000@gmail.com>
2018-10-13 16:39:03 +0200

    XML View is ready(don't know what to do with autofill warnings), starting with ui now

--- app/src/main/java/es/iessaladillo/pedrojoya/pr03/ui/main/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr03/ui/main/MainActivity.java
@@ -1,11 +1,15 @@
 package es.iessaladillo.pedrojoya.pr03.ui.main;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import es.iessaladillo.pedrojoya.pr03.R;
+import es.iessaladillo.pedrojoya.pr03.data.local.Database;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // TODO
+        ImageView profile = findViewById(R.id.imgAvatar);
+        TextView profileName = findViewById(R.id.lblAvatar);
+        setDefault(profile,profileName);
+
+
     }
 
     // DO NOT TOUCH
@@ -38,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
      **/
     private void save() {
         // TODO
-    }
 
+    }
+    private void setDefault(ImageView profile, TextView profileName){
+        profile.setImageResource(Database.getInstance().getDefaultAvatar().getImageResId());
+        profileName.setText(Database.getInstance().getDefaultAvatar().getName());
+    }
 }
